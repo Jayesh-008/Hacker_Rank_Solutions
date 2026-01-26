4a127c0e551f2d2a179cd8acc23b95a065cf8027
Jayesh-008 <jayeshsubramani008@gmail.com>
2026-01-26 10:58:21 +0530

    Add program to calculate sum and difference of numbers

---
+++ Sum_and_Difference_of_Two_Numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    int sum1,diff1;
+    float sum2,diff2;
+    scanf("%d%d",&a,&b);
+    scanf("%f%f",&c,&d);
+    sum1 = a+b;
+    diff1 = a-b;
+    sum2 = c+d;
+    diff2 = c-d;
+    printf("%d %d\n",sum1,diff1);
+    printf("%.1f %.1f\n",sum2,diff2);
+    return 0;
+}
